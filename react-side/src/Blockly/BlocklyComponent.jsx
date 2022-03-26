fab7e37ae5f630ee6d41369e7f34734ba8851dee
Krish Shah <krish.life21@gmail.com>
2022-03-26 03:25:29 -0400

    finally fucking has live updates

--- react-side/src/Blockly/BlocklyComponent.jsx
+++ react-side/src/Blockly/BlocklyComponent.jsx
@@ -21,55 +21,65 @@
  * @author samelh@google.com (Sam El-Husseini)
  */
 
-import React from 'react';
-import './BlocklyComponent.css';
+import React from "react";
+import "./BlocklyComponent.css";
 
-import Blockly from 'blockly/core';
-import locale from 'blockly/msg/en';
-import 'blockly/blocks';
+import Blockly from "blockly/core";
+import locale from "blockly/msg/en";
+import "blockly/blocks";
 
 Blockly.setLocale(locale);
 
 class BlocklyComponent extends React.Component {
-    constructor(props) {
-        super(props);
-        this.blocklyDiv = React.createRef();
-        this.toolbox = React.createRef();
+  constructor(props) {
+    super(props);
+    this.blocklyDiv = React.createRef();
+    this.toolbox = React.createRef();
+  }
+
+  componentDidMount() {
+    const { initialXml, children, ...rest } = this.props;
+    this.primaryWorkspace = Blockly.inject(this.blocklyDiv.current, {
+      toolbox: this.toolbox.current,
+      ...rest,
+    });
+
+    if (initialXml) {
+      Blockly.Xml.domToWorkspace(
+        Blockly.Xml.textToDom(initialXml),
+        this.primaryWorkspace
+      );
     }
+  }
 
-    componentDidMount() {
-        const { initialXml, children, ...rest } = this.props;
-        this.primaryWorkspace = Blockly.inject(
-            this.blocklyDiv.current,
-            {
-                toolbox: this.toolbox.current,
-                ...rest
-            },
-        );
+  get workspace() {
+    return this.primaryWorkspace;
+  }
 
-        if (initialXml) {
-            Blockly.Xml.domToWorkspace(Blockly.Xml.textToDom(initialXml), this.primaryWorkspace);
-        }
-    }
+  setXml(xml) {
+    Blockly.Xml.domToWorkspace(
+      Blockly.Xml.textToDom(xml),
+      this.primaryWorkspace
+    );
+  }
 
-    get workspace() {
-        return this.primaryWorkspace;
-    }
+  render() {
+    const { children } = this.props;
 
-    setXml(xml) {
-        Blockly.Xml.domToWorkspace(Blockly.Xml.textToDom(xml), this.primaryWorkspace);
-    }
-
-    render() {
-        const { children } = this.props;
-
-        return <React.Fragment>
-            <div ref={this.blocklyDiv} id="blocklyDiv" />
-            <xml xmlns="https://developers.google.com/blockly/xml" is="blockly" style={{ display: 'none' }} ref={this.toolbox}>
-                {children}
-            </xml>
-        </React.Fragment>;
-    }
+    return (
+      <React.Fragment>
+        <div ref={this.blocklyDiv} id="blocklyDiv" />
+        <xml
+          xmlns="https://developers.google.com/blockly/xml"
+          is="blockly"
+          style={{ display: "none" }}
+          ref={this.toolbox}
+        >
+          {children}
+        </xml>
+      </React.Fragment>
+    );
+  }
 }
 
 export default BlocklyComponent;
