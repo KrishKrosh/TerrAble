20bc6a500acb53b29d2c7744b72e1cb2ef84a011
Adrian Gri <adrian.gri03@gmail.com>
2022-03-26 04:16:49 -0400

    fixed toolbar

--- react-side/src/Blockly/BlocklyComponent.jsx
+++ react-side/src/Blockly/BlocklyComponent.jsx
@@ -38,16 +38,208 @@ class BlocklyComponent extends React.Component {
     }
 
     componentDidMount() {
+        var toolboxx = {
+            "kind": "categoryToolbox",
+            "contents": [
+              {
+                "kind": "category",
+                "name": "Control",
+                "contents": [
+                  {
+                    "kind": "block",
+                    "type": "controls_if"
+                  },
+                  {
+                    "kind": "block",
+                    "type": "logic_compare"
+                  }
+                ]
+              },
+              {
+                "kind": "category",
+                "name": "Logic",
+                "contents": [
+                  {
+                    "kind": "block",
+                    "type": "logic_compare"
+                  },
+                  {
+                    "kind": "block",
+                    "type": "logic_operation"
+                  },
+                  {
+                    "kind": "block",
+                    "type": "logic_boolean"
+                  },
+                  {
+                    "kind": "block",
+                    "type": "logic_negate"
+                  },
+                  {
+                    "kind": "block",
+                    "type": "logic_null"
+                  },
+                  {
+                    "kind": "block",
+                    "type": "logic_ternary"
+                  }
+                ]
+              },
+              {
+                "kind": "category",
+                "name": "Loops",
+                "contents": [
+                  {
+                    "kind": "block",
+                    "type": "controls_repeat_ext"
+                  },
+                  {
+                    "kind": "block",
+                    "type": "controls_whileUntil"
+                  },
+                  {
+                    "kind": "block",
+                    "type": "controls_for"
+                  },
+                  {
+                    "kind": "block",
+                    "type": "controls_forEach"
+                  },
+                  {
+                    "kind": "block",
+                    "type": "controls_flow_statements"
+                  }
+                ]
+              },
+              {
+                "kind": "category",
+                "name": "Math",
+                "contents": [
+                  {
+                    "kind": "block",
+                    "type": "math_number"
+                  },
+                  {
+                    "kind": "block",
+                    "type": "math_arithmetic"
+                  },
+                  {
+                    "kind": "block",
+                    "type": "math_single"
+                  },
+                  {
+                    "kind": "block",
+                    "type": "math_trig"
+                  },
+                  {
+                    "kind": "block",
+                    "type": "math_constant"
+                  },
+                  {
+                    "kind": "block",
+                    "type": "math_number_property"
+                  },
+                  {
+                    "kind": "block",
+                    "type": "math_round"
+                  },
+                  {
+                    "kind": "block",
+                    "type": "math_on_list"
+                  },
+                  {
+                    "kind": "block",
+                    "type": "math_modulo"
+                  },
+                  {
+                    "kind": "block",
+                    "type": "math_constrain"
+                  },
+                  {
+                    "kind": "block",
+                    "type": "math_random_int"
+                  },
+                  {
+                    "kind": "block",
+                    "type": "math_random_float"
+                  }
+                ]
+              },
+              {
+                "kind": "category",
+                "name": "Lists",
+                "contents": [
+                  {
+                    "kind": "block",
+                    "type": "lists_create_empty"
+                  },
+                  {
+                    "kind": "block",
+                    "type": "lists_create_with"
+                  },
+                  {
+                    "kind": "block",
+                    "type": "lists_repeat"
+                  },
+                  {
+                    "kind": "block",
+                    "type": "lists_length"
+                  },
+                  {
+                    "kind": "block",
+                    "type": "lists_isEmpty"
+                  },
+                  {
+                    "kind": "block",
+                    "type": "lists_indexOf"
+                  }
+                ]
+              },
+              {
+                "kind": "category",
+                "name": "Text",
+                "contents": [
+                    {
+                        "kind": "block",
+                        "type": "text_print"
+                    },
+                ]
+              },
+              {
+                "kind": "category",
+                "name": "Variables",
+                "contents": [
+                  {
+                    "kind": "button",
+                    "text": "Create variable",
+                    "callbackKey": "CREATE_VARIABLE"
+                  },
+                  {
+                    "kind": "block",
+                    "type": "variables_set"
+                  },
+                  {
+                    "kind": "block",
+                    "type": "variables_get"
+                  },
+                ]
+              }
+            ]
+          };
+
         const { initialXml, children, ...rest } = this.props;
         this.primaryWorkspace = Blockly.inject(
             this.blocklyDiv.current,
             {
-                toolbox: this.toolbox.current,
+                toolbox: toolboxx,
                 ...rest
             },
         );
+
         // changes theme to dark mode
         this.primaryWorkspace.setTheme(Blockly.Themes.Dark);
+
+        this.primaryWorkspace.registerButtonCallback("CREATE_VARIABLE", () => {console.log("fuck")});
 
         if (initialXml) {
             Blockly.Xml.domToWorkspace(Blockly.Xml.textToDom(initialXml), this.primaryWorkspace);
