0f8090e12bc16f9a22bff70cbd12377f11e75735
Karthik Nambiar <k3nambia@uwaterloo.ca>
2022-03-26 06:17:58 -0400

    added the rust transpiling

--- react-side/src/generator/generator.js
+++ react-side/src/generator/generator.js
@@ -121,3 +121,19 @@ Blockly.JavaScript['controls_if'] = function(block) {
     }
     return return_statement + '\n';
   };
+
+  Blockly.JavaScript['variables_get'] = function(block) {
+    // Variable getter.
+    var code = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('VAR'),
+        Blockly.VARIABLE_CATEGORY_NAME);
+    return ['let', Blockly.JavaScript.ORDER_ATOMIC];
+  };
+
+Blockly.JavaScript['variables_set'] = function(block) {
+    // Variable setter.
+    var argument0 = Blockly.JavaScript.valueToCode(block, 'VALUE',
+        Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
+    var varName = Blockly.JavaScript.variableDB_.getName(
+        block.getFieldValue('VAR'), Blockly.VARIABLE_CATEGORY_NAME);
+    return varName + ' = ' + argument0 + ';\n';
+  };
